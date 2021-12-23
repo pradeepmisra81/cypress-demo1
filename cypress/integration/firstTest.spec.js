13e8226ab68ccfba84acd5e80cc284584b036d6e
pradeep <pradeep.misra@gmail.com>
2021-12-23 21:23:05 +0530

    added first test structure file

---
+++ cypress/integration/firstTest.spec.js
@@ -0,0 +1,50 @@
+/// <reference types="cypress" />
+
+describe('Test Suite 1', () => {
+
+
+    describe('Test Suite Section', () => {
+
+        it('Test Case 1', () => {
+    
+            cy.visit('https://pradeepmisra81.github.io/profile-website/');
+            cy.wait(5000);
+        })
+    
+        it('Test Case 2', () => {
+    
+        })
+    
+        it('Test Case 3', () => {
+    
+        })
+    
+        it('Test Case 4', () => {
+    
+        })
+    
+        it('Test Case 5', () => {
+    
+        })
+    })
+
+    it('Test Case 1', () => {
+
+    })
+
+    it('Test Case 2', () => {
+
+    })
+
+    it('Test Case 3', () => {
+
+    })
+
+    it('Test Case 4', () => {
+
+    })
+
+    it('Test Case 5', () => {
+
+    })
+})
