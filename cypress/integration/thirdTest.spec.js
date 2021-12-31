bc9eebd69c42cb767b51ab4e79de89fa9560347d
pradeep <pradeep.misra@gmail.com>
2021-12-31 13:30:56 +0530

    Updated third Test script for ngx-admin

--- cypress/integration/thirdTest.spec.js
+++ cypress/integration/thirdTest.spec.js
@@ -1,14 +1,26 @@
 /// <reference types="cypress" />
 
+// Test Cases for ngx-admin
 describe('Test Suite 1', () => {
 
     it('Test Case 1', () => {
         cy.visit('https://akveo.github.io/ngx-admin/?utm_source=akveo_website&utm_medium=hompage&utm_campaign=github_block&__hstc=205619788.db88963c1f1e247ab3f912151e8ec9e2.1640420005059.1640420005059.1640433305997.2&__hssc=205619788.1.1640433305997&__hsfp=254261519');
+        cy.wait(5000);
     })
 
     it('Test Case 2', () => {
-        cy.get('body > ngx-landing-app > ngx-landing-pages > ngx-landing-home > nb-layout > div.scrollable-container > div > nb-layout-header > nav > ngx-landing-header > div.section.middle > div > nb-menu > ul > li.menu-item.ng-tns-c44-2.ng-star-inserted > a > span').click();
+
+        cy.get('button[class="leadinModal-close"][aria-label="Close"]').then(() => {
+            cy.get('button[class="leadinModal-close"][aria-label="Close"]').click();
+        }) 
+    })
+
+    it('Test Case 3', () => {
+
+        cy.get('span[class="menu-title ng-tns-c44-2"]').contains('Demo').click();
+
         cy.contains('Forms').click();
+
         cy.contains('Form Layouts').click();
 
         cy.get('input');
@@ -27,9 +39,7 @@ describe('Test Suite 1', () => {
 
         cy.get('input[placeholder="Email"][type="email"]#inputEmail1.input-full-width');
 
-    })
-
-    it('Test Case 3', () => {
         cy.contains('[status="warning"]','Sign in');
     })
+
 })
